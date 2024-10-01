473a444f8c0181fb0f4aec9b89e194416ef7cc1f
rarredondo9 <rebeca.arredondo.9@gmail.com>
2024-10-01 22:41:01 +0100

    MVC

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine Templates
@@ -25,10 +26,12 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-//Index Route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+/* ***********************
+ * Index Route
+ *************************/
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
